46bfde41c849fbe7fda85c44b14426bb701c3411
Ian Macalinao <me@ian.pw>
2015-05-23 13:05:07 -0700

    Add raws to get things working

--- app/src/main/java/pw/ian/vrtransit/MainActivity.java
+++ app/src/main/java/pw/ian/vrtransit/MainActivity.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.gearvrf.GVRActivity;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends GVRActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 
 
