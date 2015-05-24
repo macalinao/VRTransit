ce4bfb1314b179b97efee846246da294fbd56bad
Ian Macalinao <me@ian.pw>
2015-05-24 14:15:43 -0700

    loc

--- src/pw/ian/vrtransit/MainActivity.java
+++ src/pw/ian/vrtransit/MainActivity.java
@@ -13,11 +13,13 @@
  * limitations under the License.
  */
 
-
 package pw.ian.vrtransit;
 
 import org.gearvrf.GVRActivity;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -25,34 +27,40 @@ import android.view.MotionEvent;
 import com.firebase.client.Firebase;
 
 public class MainActivity extends GVRActivity {
-    private long lastDownTime = 0;
-    private MUNIVisualizerScript s;
-	
-    @Override
-    protected void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
-        Firebase.setAndroidContext(this);
-        s = new MUNIVisualizerScript(this);
-        setScript(s, "gvr_note4.xml");
-        Log.i("VRTransit", "Init application");
-    }
-    
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        Log.i("VRTransit", "Touch received");
-        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-            lastDownTime = event.getDownTime();
-        }
+	private long lastDownTime = 0;
+	private MUNIVisualizerScript s;
 
-        if (event.getActionMasked() == MotionEvent.ACTION_UP) {
-            // check if it was a quick tap
-            if (event.getEventTime() - lastDownTime < 200) {
-            	s.handleTap();
-            } else {
-            	s.handleLongPress();
-            }
-        }
+	@Override
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
 
-        return true;
-    }
+		s = new MUNIVisualizerScript(this);
+//		LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//		s.updateLocation(mLocationManager
+//				.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+//		Firebase.setAndroidContext(this);
+		setScript(s, "gvr_note4.xml");
+		Log.i("VRTransit", "Init application");
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (s == null)
+			return true;
+		Log.i("VRTransit", "Touch received");
+		if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+			lastDownTime = event.getDownTime();
+		}
+
+		if (event.getActionMasked() == MotionEvent.ACTION_UP) {
+			// check if it was a quick tap
+			if (event.getEventTime() - lastDownTime < 200) {
+				s.handleTap();
+			} else {
+				s.handleLongPress();
+			}
+		}
+
+		return true;
+	}
 }
