0d772291465b4385ef241d340209820e6e9c77a2
Ian Macalinao <me@ian.pw>
2015-05-23 17:57:55 -0700

    Add some firebase code

---
+++ src/pw/ian/vrtransit/data/BusUpdate.java
@@ -0,0 +1,35 @@
+package pw.ian.vrtransit.data;
+
+public class BusUpdate {
+	private String route;
+
+	private double lat;
+
+	private double lon;
+
+	private String type;
+
+	public BusUpdate(String route, double lat, double lon, String type) {
+		this.route = route;
+		this.lat = lat;
+		this.lon = lon;
+		this.type = type;
+	}
+
+	public String getRoute() {
+		return route;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+}
---
+++ src/pw/ian/vrtransit/data/TransitDataAccessor.java
@@ -0,0 +1,64 @@
+package pw.ian.vrtransit.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+public class TransitDataAccessor {
+	private Firebase ref = new Firebase(
+			"https://publicdata-transit.firebaseio.com/sf-muni/vehicles");
+
+	private ArrayBlockingQueue<BusUpdate> pendingUpdates = new ArrayBlockingQueue<>(
+			10000);
+
+	public TransitDataAccessor() {
+
+		ref.addChildEventListener(new ChildEventListener() {
+
+			@Override
+			public void onCancelled(FirebaseError arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onChildAdded(DataSnapshot arg0, String arg1) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onChildChanged(DataSnapshot ds, String prevKey) {
+				String route = ds.child("routeTag").getValue(String.class);
+				double lat = ds.child("lat").getValue(Double.class);
+				double lon = ds.child("lon").getValue(Double.class);
+				String type = ds.child("type").getValue(String.class);
+				pendingUpdates.add(new BusUpdate(route, lat, lon, type));
+			}
+
+			@Override
+			public void onChildMoved(DataSnapshot arg0, String arg1) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onChildRemoved(DataSnapshot arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
+
+	public List<BusUpdate> nextUpdates() {
+		List<BusUpdate> ret = new ArrayList<>();
+		pendingUpdates.drainTo(ret);
+		return ret;
+	}
+}
